73d2465cf528bb7407d6258369900dc429046f6b
Caleb-Oneyemi <caloneyemi@gmail.com>
2021-08-16 08:41:21 +0100

    initial commit

---
+++ posts/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const posts = {};
+
+app.post("/posts/create", async (req, res) => {
+	const id = randomBytes(4).toString("hex");
+	const { title } = req.body;
+
+	posts[id] = {
+		id,
+		title,
+	};
+
+	try {
+		await axios.post("http://event-bus-srv:4005/events", {
+			type: "PostCreated",
+			data: {
+				id,
+				title,
+			},
+		});
+	} catch (err) {
+		res.status(400).json({ error: 'Error reaching eventbus' });
+	}
+
+	res.status(201).send(posts[id]);
+});
+
+app.post("/events", (req, res) => {
+	console.log("Received Event", req.body.type);
+
+	res.send({});
+});
+
+app.listen(4000, () => {
+	console.log("Listening on 4000.....");
+});
